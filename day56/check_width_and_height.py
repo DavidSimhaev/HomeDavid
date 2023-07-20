4e63a914ea530fd7eaaccd0df266a016a5f744e4
DavidSimhaev <davidlung822@gmail.com>
2023-07-21 02:37:37 +0300

    PROJECT_UPDATE

--- day56/check_width_and_height.py
+++ day56/check_width_and_height.py
@@ -1,5 +1,5 @@
 from PIL import Image
-
+import os
 class Image_ch:
     
     new_width  = 500
@@ -9,10 +9,11 @@ class Image_ch:
         self.image = Image.open(image)
         self.new_width  = 500
         self.new_height = 500
+        self.url_l = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
     
     
     def save(self):
-        img = self.image.resize((self.new_width, self.new_height), Image.ANTIALIAS)
+        img = self.image.resize((self.new_width, self.new_height), Image.LANCZOS)
         x = []
         for elem in self.url[::-1]:
             if elem != "/":
@@ -21,5 +22,5 @@ class Image_ch:
             else:
                 break 
         res = "".join(x[::-1])
-        img.save("C:/Users/ASUS/Desktop/HomeWorkDavid/day56/image/image_pr/"+res)
-        return "C:/Users/ASUS/Desktop/HomeWorkDavid/day56/image/image_pr/"+res
+        img.save(f"{self.url_l}/image/image_pr/"+res)
+        return f"{self.url_l}/image/image_pr/"+res
