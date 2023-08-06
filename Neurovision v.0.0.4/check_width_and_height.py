052ffb2b6893d180b57d7c6d16eb2da7348c5c5e
David <davidlung822@gmail.com>
2023-08-06 18:58:20 +0300

    PR_v.04

---
+++ Neurovision v.0.0.4/check_width_and_height.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import os
+class Image_ch:
+    
+    new_width  = 500
+    new_height = 500
+    def __init__(self, image, name_pr) -> None:
+        self.name_pr = name_pr
+        self.url = image
+        self.image = Image.open(image)
+        self.new_width  = 500
+        self.new_height = 500
+        self.url_l = os.path.dirname(os.path.abspath(__file__)).replace("\\", "/")
+
+    def save(self):
+        img = self.image.resize((self.new_width, self.new_height), Image.Resampling.LANCZOS)
+        x = []
+        for elem in self.url[::-1]:
+            if elem != "/":
+                x.extend(elem)
+                continue
+            else:
+                break 
+        res = "".join(x[::-1])
+        
+        file = f"{self.url_l}/projects/{self.name_pr}/"+res
+        
+        result = file[:-3] + "png" 
+          
+        img.save(result)
+        
+        
+        return result
